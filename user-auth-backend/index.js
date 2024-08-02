0bc8ec6dfaeb0910e8101b6c4a0a5a2c76dd14d6
Harshiiet Negi <negeek7@gmail.com>
2024-08-02 13:06:30 +0530

    nodemon + gitignore changes

--- user-auth-backend/index.js
+++ user-auth-backend/index.js
@@ -1,6 +1,8 @@
-import {express} from express;
+
+import express from "express";
 
 const app = express()
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
     console.log(`lisening on prt ${PORT}`)
