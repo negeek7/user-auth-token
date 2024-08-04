5e376c28e2d3466e1a701f719c06c30d4182e891
Harshiiet Negi <negeek7@gmail.com>
2024-08-04 14:48:01 +0530

    create user, checking permissions with jwt

--- user-auth-backend/index.js
+++ user-auth-backend/index.js
@@ -61,18 +61,18 @@ app.post('/signin', async (req, res) => {
     // get hashedPassword
     // match with the input user password
     try {
-        let {username, password} = req.body
-        if(!username || !password) return res.status(400).send("Username or password missing!")
-        let user = await User.findOne({username: {$eq: username}}, {created_at: 0})
+        let { username, password } = req.body
+        if (!username || !password) return res.status(400).send("Username or password missing!")
+        let user = await User.findOne({ username: { $eq: username } }, { created_at: 0 })
 
-        if(!user) return res.status(404).send("User does not exist!")
+        if (!user) return res.status(404).send("User does not exist!")
 
         let passwordResult = await bcrypt.compare(password, user.password)
-        if(passwordResult) {
-            let token = jwt.sign({username: user.username, role: user.role ? user.role : ''}, process.env.JWT_SECRET_KEY, {
+        if (passwordResult) {
+            let token = jwt.sign({ username: user.username, role: user.role ? user.role : '' }, process.env.JWT_SECRET_KEY, {
                 expiresIn: '30m'
             })
-            return res.status(200).json({message: "Authenticated", token})
+            return res.status(200).json({ message: "Authenticated", token })
         } else {
             return res.status(401).send("Wrong password! Check again.")
         }
@@ -87,20 +87,29 @@ app.post('/signin', async (req, res) => {
 // create a user (only user with role admin can create user)
 
 app.post('/createuser', async (req, res) => {
-    let {username} = req.body
-    if(!username) return res.status(400).send("Please provide username")
-        try {
+    let { username } = req.body
+    if (!username) return res.status(400).send("Please provide username")
+    try {
+        let user = User.findOne({ username: { $eq: username } })
+        if (!user) return res.status(404).send("User does not exist!")
         let usertoken = req.headers.authorization.split(' ')[1]
-        if(usertoken) {
+        if (usertoken) {
             let userInfo = jwt.decode(usertoken)
-            if(userInfo.role == 'admin') {
-                let newUser = new User({
-                    username: username,
-                    password: "randomPassword123",
+            if (userInfo.role == 'admin') {
+                let saltRounds = 10;
+                let salt = await bcrypt.genSalt(saltRounds);
+                let password = 'randompassword123'
+                let hashedPassword = await bcrypt.hash(password, salt);
+                const user = new User({
+                    username,
+                    password: hashedPassword,
                     role: 'user'
                 })
-                await newUser.save()
-                res.status(200).json({message: "User created!", user: {name: newUser.username, role: newUser.role}})
+                await user.save()
+                res.status(200).send("User successfully created!")
+            } else {
+                res.status(400).send("You do not have permission to create a user!")
+
             }
         }
     } catch (error) {
--- user-auth-backend/utils/utils.js
+++ user-auth-backend/utils/utils.js
@@ -1,4 +1,14 @@
+import jwt from 'jsonwebtoken'
+
 export function handleApiError(res, error, responseMessage, message){
     console.log(message, error)
     return res.status(500).send(responseMessage)
+}
+
+export function createToken(payload, expiresIn = '30m') {
+    let {username, role} = payload
+    let token = jwt.sign({username, role}, process.env.JWT_SECRET_KEY, {
+        expiresIn: expiresIn
+    })
+    return token
 }
