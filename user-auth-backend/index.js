ce5733d364fbae9b16b92e65536e0aee70a23fb4
Harshiiet Negi <negeek7@gmail.com>
2024-08-27 16:16:06 +0530

    fixing api errors and response

--- user-auth-backend/index.js
+++ user-auth-backend/index.js
@@ -70,7 +70,7 @@ app.post('/api/signup', async (req, res) => {
             }
         }
         // sign up user
-        if (!username || !password) return res.status(400).json({status: "Error", message: "Username or password is missing!"});
+        if (!username || !password) return res.status(400).json({ status: "Error", message: "Username or password is missing!" });
         return createUser(username, password, 'admin', res)
     } catch (error) {
         console.log("Error while sign up!", error)
@@ -91,7 +91,7 @@ async function createUser(username, password, role, res) {
             role: role
         })
         await newUser.save()
-        return res.status(200).json({status: "Success", message: "User successfully created!"})
+        return res.status(200).json({ status: "Success", message: "User successfully created!" })
     } catch (error) {
         console.log("Error while sign up!", error)
         return handleApiError(res, error, "Trouble creating user", "sign up api error")
@@ -106,27 +106,28 @@ app.post('/api/signin', async (req, res) => {
     // match with the input user password
     try {
         let { username, password } = req.body
-        if (!username || !password) return res.status(400).send("Username or password missing!")
+        if (!username || !password) return res.status(400).json({
+            staus: "Error",
+            message: "Username or password missing!"
+        })
         let user = await User.findOne({ username: { $eq: username } }, { created_at: 0 })
 
-        if (!user) return res.status(404).send("User does not exist!")
-
-        console.log(user, "user found")
+        if (!user) return res.status(404).json({ status: "Invalid", message: "User does not exist" })
 
         let passwordResult = await bcrypt.compare(password, user.password)
         if (passwordResult) {
-            let token = jwt.sign({ username: user.username, role: user.role ? user.role : '' }, process.env.JWT_SECRET_KEY, {
-                expiresIn: '30m'
-            })
-            res.cookie('authToken', token, {httpOnly: true, maxAge: 900000})
+            // let token = jwt.sign({ username: user.username, role: user.role ? user.role : '' }, process.env.JWT_SECRET_KEY, {
+            //     expiresIn: '30m'
+            // })
+            // res.cookie('authToken', token, {httpOnly: true, maxAge: 900000})
             return res.status(200).json({
                 message: "Authenticated", user: {
                     username,
-                    token
+                    token: "123"
                 }
             })
         } else {
-            return res.status(401).send("Wrong password! Check again.")
+            return res.status(401).json({ status: "Invalid", message: "Wrong password! Check again" })
         }
     } catch (error) {
         handleApiError(res, error, "Trouble signing in user", "sign in api error")
--- user-auth-frontend/src/pages/Login.jsx
+++ user-auth-frontend/src/pages/Login.jsx
@@ -24,7 +24,6 @@ function Login() {
     const handleSubmit = async () => {
         if (tabState === 'login') {
             let response = await apiCaller('api/signin', 'POST', formInfo)    
-            console.log(response, "response")
             if(response.message == "Authenticated") {
                 navigate('/home') 
             }
