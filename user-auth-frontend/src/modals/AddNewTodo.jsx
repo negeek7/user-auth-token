27a4c05a7b339d458107d1118af2977cc4901e3d
Harshiiet Negi <negeek7@gmail.com>
2024-08-27 18:15:24 +0530

    todo modal add items

--- user-auth-frontend/src/modals/AddNewTodo.jsx
+++ user-auth-frontend/src/modals/AddNewTodo.jsx
@@ -1,61 +1,48 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function AddNewTodo({onClose}) {
+
+
+    const listRef = useRef(null)
+
+
+
+
+    const handleAddNewItems = () => {
+        // add an input box
+        console.log(listRef, "LIST REFERENCE");
+        let itemInput = document.createElement('input');
+        // itemInput.style.padding = "2px";
+        listRef.current.appendChild(itemInput)
+
+    }
     return (
         <>
-            <>
                 <div
                     className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
                 >
-                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                        {/*content*/}
-                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-gray-600 outline-none focus:outline-none">
-                            {/*header*/}
-                            <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                                <h3 className="text-3xl font-semibold">
-                                    Modal Title
-                                </h3>
-                                <button
-                                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                                    onClick={() => setShowModal(false)}
-                                >
-                                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                                        ×
-                                    </span>
-                                </button>
-                            </div>
-                            {/*body*/}
-                            <div className="relative p-6 flex-auto">
-                                <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
-                                    I always felt like I could do anything. That’s the main
-                                    thing people are controlled by! Thoughts- their perception
-                                    of themselves! They're slowed down by their perception of
-                                    themselves. If you're taught you can’t do anything, you
-                                    won’t do anything. I was taught I could do everything.
-                                </p>
-                            </div>
-                            {/*footer*/}
-                            <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                <button
-                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                    type="button"
-                                    onClick={onClose}
-                                >
-                                    Close
-                                </button>
-                                <button
-                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                                    type="button"
-                                    onClick={onClose}
-                                >
-                                    Save Changes
-                                </button>
-                            </div>
+                    <div className="border-0 h-96 w-3/12 bg-gray-900 rounded-xl p-4">
+                        {/* title */}
+                        <p>Make a new Todo</p>
+                        <input placeholder="What are we planning?" className="mt-4 bg-gray-800 border-none focus:outline-none p-2 w-full rounded-lg"/>
+
+                        <div ref={listRef} className="mt-2 flex flex-col gap-2  child:rounded-lg child:outline-none child:px-2 child:py-1 border border-red-400">
                         </div>
+
+                        {/* create list of items (of todos) */}
+
+                        <button onClick={handleAddNewItems} className="w-full border border-dotted border-gray-700 h-12 flex items-center justify-center mt-4 text-lg focus:outline-none"> + </button>
+
+
+
+
+
+
+
+                        {/* close || save changes */}
                     </div>
                 </div>
                 <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-            </>
         </>
     );
 }
