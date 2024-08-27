c9f763298309b36bdc0836540f286d9bc5d973f5
Harshiiet Negi <negeek7@gmail.com>
2024-08-27 17:06:46 +0530

    add todo modal

--- user-auth-frontend/src/pages/Home.jsx
+++ user-auth-frontend/src/pages/Home.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { createPortal } from 'react-dom';
+import AddNewTodo from '../modals/AddNewTodo';
 
 function Home() {
 
 
+  // Show user notes
+  // User to add new notes
     
+  const [addNewTodo, setAddNewTodo] = useState(false)
 
 
   return (
-    <div>This is Home Page</div>
+    <div className="m-10">
+      <div className='h-28 w-28 border border-red-500 flex justify-center items-center cursor-pointer' onClick={() => setAddNewTodo(true)}> 
+        +
+      </div>
+
+      {
+        addNewTodo && createPortal(<AddNewTodo onClose={() => setAddNewTodo(false)}/>, document.body)
+      }
+    </div>
   )
 }
 
